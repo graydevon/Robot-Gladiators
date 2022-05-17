cabcf5327f72e1fb3c35863302ba753114d8d28e
Devon <devondgray28@gmail.com>
2022-05-17 16:21:12 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,22 @@ var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 12; 
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length)
+
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
+
+
+//fight(enemyRobot);
+
+
 
 // enemy robot names 
 var enemy1 = "Roborto";
@@ -28,10 +42,10 @@ var enemy2 = "Amy Android";
 var enemy3 = "Robo Trumble";
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
-
+}
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -66,7 +80,7 @@ var fight = function() {
   } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
+  
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
@@ -80,8 +94,14 @@ var fight = function() {
     // if player did not chose 1 or 2 in prompt
   } else {
     window.alert("You need to pick a valid option. Try again!");
+
+    for(var i = 0; i < enemyNames.length; i++) {
+      console.log(enemyNames[i]);
+
   }
 }; // end of fight function
 
 // run fight function to start game
-//fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]); 
+}
