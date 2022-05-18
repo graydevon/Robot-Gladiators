3be3db80a2ae18b044269c1c05e9089a3f49782b
Devon <devondgray28@gmail.com>
2022-05-17 19:23:23 -0500

    debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -42,10 +42,10 @@ var enemy2 = "Amy Android";
 var enemy3 = "Robo Trumble";
 
 // fight function
-var fight = function(enemyName) {
+//var fight = function(enemyName) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-}
+  //window.alert("Welcome to Robot Gladiators!");
+//}
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -54,7 +54,7 @@ var fight = function(enemyName) {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
-      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+      playerName + " attacked " + edcXnemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
     // check enemy's health
@@ -69,7 +69,7 @@ var fight = function(enemyName) {
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
-
+    
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
@@ -94,7 +94,8 @@ var fight = function(enemyName) {
     // if player did not chose 1 or 2 in prompt
   } else {
     window.alert("You need to pick a valid option. Try again!");
-
+     
+    debugger;
     for(var i = 0; i < enemyNames.length; i++) {
       console.log(enemyNames[i]);
 
@@ -102,6 +103,12 @@ var fight = function(enemyName) {
 }; // end of fight function
 
 // run fight function to start game
+
+var fight = function(enemyName) {
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) 
+}
+  
 for(var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]); 
 }
